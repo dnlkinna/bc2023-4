80c5a10d46651f0225e1a0777f4aa6d92a9e6e5d
dnlkinna <145540174+dnlkinna@users.noreply.github.com>
2023-11-22 08:57:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+//підключаємо модуль http
+const http = require('http');
 const { XMLParser, XMLBuilder} = require("fast-xml-parser");
 const fs = require('fs');
-
-const xmlData = fs.readFileSync('data.xml', 'utf8');
+//створюємо об'єкт сервера
+const server = http.createServer((req, res) => {
+    const xmlData = fs.readFileSync('data.xml', 'utf8');
 
 const parser = new XMLParser();
 var jObj = parser.parse(xmlData);
@@ -23,4 +26,13 @@ const xmlOptions = {
   };
 const builder = new XMLBuilder(xmlOptions);
 const xmlContent = builder.build(data);
-console.log(xmlContent);
+
+res.writeHead(200, { 'Content-Type': 'application/xml' });
+//завершуємо відповідь
+res.end(xmlContent);
+
+});
+
+server.listen(8080, () => {
+  console.log('Server has been started at 8080 port');
+});
